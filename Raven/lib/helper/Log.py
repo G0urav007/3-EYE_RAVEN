3000d0bbba5cdd4b22333dcb725ff29d7c4f332f
Gourav Sankalle <82142638+G0urav007@users.noreply.github.com>
2023-04-11 02:47:29 -0400

    Add files via upload

---
+++ Raven/lib/helper/Log.py
@@ -0,0 +1,20 @@
+'''
+Tool-2023
+
+'''
+from lib.helper.helper import * 
+from datetime import datetime
+class Log:
+
+	@classmethod
+	def info(self,text):
+ 		print("["+Y+datetime.now().strftime("%H:%M:%S")+N+"] ["+G+"INFO"+N+"] "+text)
+ 
+	@classmethod
+	def warning(self,text):
+		print("["+Y+datetime.now().strftime("%H:%M:%S")+N+"] ["+Y+"WARNING"+N+"] "+text)
+
+	@classmethod
+	def high(self,text):
+ 		print("["+Y+datetime.now().strftime("%H:%M:%S")+N+"] ["+R+"CRITICAL"+N+"] "+text)
+ 		
